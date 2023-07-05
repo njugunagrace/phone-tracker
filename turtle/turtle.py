73c930f785c75601a85749cf471c9080d76bbf55
Grace <njugunagraceewambui@gmail.com>
2023-07-05 08:06:03 +0300

    a phone tracker

---
+++ turtle/turtle.py
@@ -0,0 +1,16 @@
+import turtle
+from freegames import turtle
+
+turtle.bgcolor("pink")
+turtle.pensize(2.5)
+turtle.speed(0.5)
+color = ["red","green","yellow","blue"]
+for x in range(9):
+    for i in color:
+        turtle.color(i)
+        turtle.circle(150)
+        turtle.left(10)
+turtle.mainloop()
+
+
+        
